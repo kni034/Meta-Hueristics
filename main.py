b80ebb9a17cd5faaf31f85fe02538e28b126692c
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2020-03-15 15:30:41 +0100

    temp commit

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import readfile
 
 readfile.read()
 
-a=readfile.gen_random_solution()
-print(a)
-for i in range(1000):
-    a=readfile.gen_random_solution()
-    print(a)
+
+readfile.test()
--- readfile.py
+++ readfile.py
@@ -24,7 +24,7 @@ def read(filename = 'Call_7_Vehicle_3.txt'):
         f.readline()
 
         #adds all vehicles start node to a dictionary with lists
-        #vehicle_start = {vehicle index:[home node, starting time, capacity]}
+        #vehicle_start = { vehicle index:[(0)home node, (1)starting time, (2)capacity]}
         global vehicle_start
         for _ in range(num_vehicles):
             line = f.readline().rstrip('\n')
@@ -41,7 +41,9 @@ def read(filename = 'Call_7_Vehicle_3.txt'):
         f.readline()
 
         #list of calls each vehicle can do
-        #vehicle_calls = {vehicle number:[call1, call3...]}
+        #vehicle_calls = {vehicle number:[call]}
+        #call = (0)vehicle index, 
+        # (1)list of calls that can be transported using that vehicle
         vehicle_calls={}
         for _ in range(num_vehicles):
             line = f.readline().rstrip('\n')
@@ -54,6 +56,10 @@ def read(filename = 'Call_7_Vehicle_3.txt'):
 
         #adds all calls to a list containing the callinfo in a dictionary
         #calls = {call_index : [call]}
+        #call = (0)call index, (1)origin node, (2)destination node, (3)size, 
+        # (4)cost of not transporting, (5)lowerbound timewindow for pickup, 
+        # (6)upper_timewindow for pickup, (7)lowerbound timewindow for delivery, 
+        # (8)upper_timewindow for delivery
         global calls
         for _ in range(num_calls):
             line = f.readline().rstrip('\n')
@@ -66,7 +72,7 @@ def read(filename = 'Call_7_Vehicle_3.txt'):
 
         #travel times and cost in a dict with tupel as key
         #travel_times_and_cost = 
-        # {(vehicle number, origin node, destinaton node) : [travel time, travel cost]} 
+        # {(vehicle number, origin node, destinaton node) : [(0)travel time, (1)travel cost]} 
         global travel_times_and_cost
         for _ in range(num_nodes*num_nodes*num_vehicles):
             line = f.readline().rstrip('\n')
@@ -83,7 +89,7 @@ def read(filename = 'Call_7_Vehicle_3.txt'):
         #time and cost of picking up and dropping packages in a call
         # node_time_and_cost = 
         # {(vechicle number, call) : 
-        # (origin node time, origin node cost, destination node time, destination node cost)}
+        # ((0)origin node time, (1)origin node cost, (2)destination node time, (3)destination node cost)}
         global node_time_and_cost
         for _ in range(num_calls * num_vehicles):
             line = f.readline().rstrip('\n')
@@ -157,10 +163,71 @@ def gen_random_solution():
     return solution
 
 
+def test():
+    car_id = 1
+    time = vehicle_start[car_id][1]
+    print(time)
+
+
 def check_feasibility(solution):
 
     car_id = 1
+    car_weight = 0
+    time = vehicle_start[car_id][1]
+    unfinished_calls = []
+    unfinished_calls.extend(range(1, num_calls+1))
+    cars_storage = []
+    last_node = vehicle_start[car_id][0]
 
-    
+
+    if len(solution) is not (num_calls * 2) + num_vehicles:
+        print("solution har feil lengde")
+        return False
+
     for call in solution:
+        #check for undelivered calls and change and reset active cars stats
         if call == 0:
+            
+            if len(cars_storage) is not 0:
+                return False
+
+            car_id += 1
+            car_weight = 0
+            time = vehicle_start[car_id][1]
+            last_node = vehicle_start[car_id][0]
+
+        #number is not 0 so it is a call and not a change of car
+        else:
+            #check if a call is 'visited' more than 2 times
+            try:
+                if call in cars_storage:
+                    cars_storage.remove(call)
+                    unfinished_calls.remove(call)
+                    
+                    car_weight += calls[call][3]
+
+
+
+                else:
+                    cars_storage.append(call)
+
+                    car_weight -= calls[call][3]
+                
+            except:
+                print("fler enn 2 av samme call")
+                return False
+
+            #check if package exceeds cars capacity
+
+            if car_weight >= vehicle_start[car_id][2]:
+                print("over papasitet for bil nr.", car_id, " på call: ", call)
+                return False
+
+            #calculate and check time usage
+            
+
+            
+            #check if car can do call
+
+    return True
+            
